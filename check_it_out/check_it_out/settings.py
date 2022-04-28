2dc887a0b1a390bdeb5679f0f7b2a95055196d8c
Dhakshinesh <dhakshineshanandh@gmail.com>
2022-04-28 17:27:00 +0530

    Created MySQL-Database✅

--- check_it_out/check_it_out/settings.py
+++ check_it_out/check_it_out/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,11 @@ WSGI_APPLICATION = 'check_it_out.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Check_it_out_DB',
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': '127.0.0.1'
     }
 }
 
